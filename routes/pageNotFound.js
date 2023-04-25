18abb1b5ad1bfb18df191317ee18ecc2c90c0dc5
Dmitriy Tipsin <tipsin.dm@yandex.ru>
2023-04-25 19:12:27 +0300

    feat: Создание, обновление, вывод пользовательских данных, Создание, обновление карточек

--- routes/pageNotFound.js
+++ routes/pageNotFound.js
@@ -1,5 +1,6 @@
-const {pageNotFound} = require("../controllers/pageNotFound");
 const router = require('express').Router();
+const { pageNotFound } = require('../controllers/pageNotFound');
+
 router.get('/*', pageNotFound);
 
 module.exports = router;
