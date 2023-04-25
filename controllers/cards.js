3091d08dc1d397e8d0667b8b631e786a8945adef
Dmitriy Tipsin <tipsin.dm@yandex.ru>
2023-04-25 17:12:26 +0300

    feat: Создание, обновление, вывод пользовательских данных, Создание, обновление карточек

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,20 +1,20 @@
 const Card = require('../models/card');
-const {errorHandler} = require('../utils/utils');
+const { errorHandler } = require('../utils/utils');
 
 module.exports.getAllCards = (req, res) => {
   Card.find()
     .orFail()
-    .then((cards) => res.send({cards}))
+    .then((cards) => res.send({ cards }))
     .catch((err) => errorHandler(err, res));
 };
 
 module.exports.createCard = (req, res) => {
-  const {name, link} = req.body;
-  const {_id} = req.user;
+  const { name, link } = req.body;
+  const { _id } = req.user;
 
-  Card.create({name, link, owner: _id})
+  Card.create({ name, link, owner: _id })
     .then((card) => card.populate('owner'))
-    .then((card) => res.send({card}))
+    .then((card) => res.send({ card }))
     .catch((err) => errorHandler(err, res));
 };
 
@@ -22,31 +22,31 @@ module.exports.deleteCard = (req, res) => {
   console.log('Delete Card');
   const _id = req.params.cardId;
   console.log(_id);
-  Card.deleteOne({_id})
+  Card.deleteOne({ _id })
     .orFail()
-    .then((cards) => res.send({cards}))
+    .then((cards) => res.send({ cards }))
     .catch((err) => errorHandler(err, res));
 };
 
 module.exports.likeCard = (req, res) => {
-  const {_id} = req.user;
+  const { _id } = req.user;
   Card.findByIdAndUpdate(
     req.params.cardId,
-    {$addToSet: {likes: _id}},
-    {new: true},
+    { $addToSet: { likes: _id } },
+    { new: true },
   ).orFail()
     .populate('likes')
-    .then((cards) => res.send({cards}))
+    .then((cards) => res.send({ cards }))
     .catch((err) => errorHandler(err, res));
 };
 
 module.exports.dislikeCard = (req, res) => {
-  const {_id} = req.user;
+  const { _id } = req.user;
   Card.findByIdAndUpdate(
     req.params.cardId,
-    {$pull: {likes: _id}},
-    {new: true},
+    { $pull: { likes: _id } },
+    { new: true },
   ).orFail()
-    .then((cards) => res.send({cards}))
+    .then((cards) => res.send({ cards }))
     .catch((err) => errorHandler(err, res));
 };
--- controllers/users.js
+++ controllers/users.js
@@ -1,44 +1,44 @@
 const User = require('../models/user');
-const {errorHandler} = require('../utils/utils');
+const { errorHandler } = require('../utils/utils');
 
 module.exports.getUser = (req, res) => {
-  const {_id} = req.user;
+  const { _id } = req.user;
 
-  User.findById({_id})
+  User.findById({ _id })
     .orFail()
-    .then((user) => res.send({user}))
+    .then((user) => res.send({ user }))
     .catch((err) => errorHandler(err, res));
 };
 
 module.exports.getAllUsers = (req, res) => {
   User.find({})
     .orFail()
-    .then((user) => res.send({user}))
+    .then((user) => res.send({ user }))
     .catch((err) => errorHandler(err, res));
 };
 
 module.exports.createUser = (req, res) => {
-  const {name, about, avatar} = req.body;
+  const { name, about, avatar } = req.body;
 
-  User.create({name, about, avatar})
-    .then((user) => res.status(201).send({user}))
+  User.create({ name, about, avatar })
+    .then((user) => res.status(201).send({ user }))
     .catch((err) => errorHandler(err, res));
 };
 
 module.exports.updateProfileUser = (req, res) => {
-  const {name, about} = req.body;
-  const {_id} = req.user;
+  const { name, about } = req.body;
+  const { _id } = req.user;
 
-  User.findByIdAndUpdate(_id, {name, about})
-    .then((user) => res.send({user}))
+  User.findByIdAndUpdate(_id, { name, about })
+    .then((user) => res.send({ user }))
     .catch((err) => errorHandler(err, res));
 };
 
 module.exports.updateAvatarUser = (req, res) => {
-  const {avatar} = req.body;
-  const {_id} = req.user;
+  const { avatar } = req.body;
+  const { _id } = req.user;
 
-  User.findByIdAndUpdate(_id, {avatar})
-    .then((user) => res.send({user}))
+  User.findByIdAndUpdate(_id, { avatar })
+    .then((user) => res.send({ user }))
     .catch((err) => errorHandler(err, res));
 };
