d3ba754f3997be2aca8ad90f62b93c208453401b
Dmitriy Tipsin <tipsin.dm@yandex.ru>
2023-05-07 22:17:01 +0300

    feat: Практическая работа №14

--- models/user.js
+++ models/user.js
@@ -48,21 +48,23 @@ const userSchema = new mongoose.Schema({
   },
 }, { versionKey: false });
 
-userSchema.statics.findUserByCredentials = (email, password) => this.findOne({ email })
-  .select('+password')
-  .then((user) => {
-    if (!user) {
-      return Promise.reject(new NotFound('Неправильные почта или пароль'));
-    }
+userSchema.statics.findUserByCredentials = function findUserByCredentials(email, password) {
+  return this.findOne({ email })
+    .select('+password')
+    .then((user) => {
+      if (!user) {
+        return Promise.reject(new NotFound('Неправильные почта или пароль'));
+      }
 
-    return bcrypt.compare(password, user.password)
-      .then((matched) => {
-        if (!matched) {
-          return Promise.reject(new NotFound('Неправильные почта или пароль'));
-        }
+      return bcrypt.compare(password, user.password)
+        .then((matched) => {
+          if (!matched) {
+            return Promise.reject(new NotFound('Неправильные почта или пароль'));
+          }
 
-        return user;
-      });
-  });
+          return user;
+        });
+    });
+};
 
 module.exports = mongoose.model('user', userSchema);
