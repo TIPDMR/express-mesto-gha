40a1b4c76470e31a226e00870b2d601735ba4b82
Dmitriy Tipsin <tipsin.dm@yandex.ru>
2023-05-20 14:46:07 +0300

    refactor: Изменение имени домена для CORS

--- utils/constants.js
+++ utils/constants.js
@@ -2,7 +2,7 @@ module.exports.URI_REGEX = /^(http|https):\/\/[^ "]+$/;
 // Значение для заголовка Access-Control-Allow-Methods по умолчанию (разрешены все типы запросов)
 module.exports.DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
 module.exports.ALLOWED_DOMAIN_NAME = [
-  'https://movies-explorer-frontend-henna.vercel.app/',
-  'http://movies-explorer-frontend-henna.vercel.app/',
+  'https://best-mesto.ru/',
+  'http://best-mesto.ru/',
   'http://localhost:3000',
 ];
